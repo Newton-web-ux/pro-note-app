4ef739c8e6e9f880730e4a62c94fed28b06b96cb
Newton-web-ux <banerjeearnab.live@gmail.com>
2026-06-25 21:25:45 +0530

    Implement service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+const CACHE_NAME="pro-document-v1";
+
+
+const filesToCache=[
+"./",
+"./index.html",
+"./manifest.json"
+];
+
+
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+return cache.addAll(filesToCache);
+})
+
+);
+
+});
+
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
+
+
+self.addEventListener("activate",event=>{
+
+event.waitUntil(
+
+caches.keys().then(keys=>{
+
+return Promise.all(
+
+keys.map(key=>{
+
+if(key!==CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+});
