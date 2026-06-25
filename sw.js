d555ebfa9c8efcb01c50016df3106bbc932b4635
Newton-web-ux <banerjeearnab.live@gmail.com>
2026-06-25 21:50:10 +0530

    Refactor service worker for caching and event handling

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "pro-document-v1";
+
+const files = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then(cache => cache.addAll(files))
+  );
+});
+
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => response || fetch(event.request))
+  );
+});
